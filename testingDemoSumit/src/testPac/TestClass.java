6a10e10952d99488d54f2e49f74ae332c1d90947
summitbarman <summitbarman87@gmail.com>
2017-05-21 14:22:53 +0530

    Update TestClass.java

--- testingDemoSumit/src/testPac/TestClass.java
+++ testingDemoSumit/src/testPac/TestClass.java
@@ -5,5 +5,6 @@ public class TestClass
 	public static void main(String[] args) 
 	{
 		//System.out.println("Hi! Hello World");
+		System.out.println("I have Edited this statement inside git without importing!");
 	}
 }
